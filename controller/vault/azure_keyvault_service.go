e4cfd21fa4e53d1b93debfbf6414d224f82742d0
Jon Arild Tørresdal <jon.torresdal@spv.no>
2019-02-01 14:26:56 +0100

    Refactored

--- controller/vault/azure_keyvault_service.go
+++ controller/vault/azure_keyvault_service.go
@@ -5,11 +5,8 @@ import (
 	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/2016-10-01/keyvault"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/adal"
 	"github.com/Azure/go-autorest/autorest/azure/auth"
 	azureKeyVaultSecretv1alpha1 "github.com/SparebankenVest/azure-keyvault-controller/pkg/apis/azurekeyvaultcontroller/v1alpha1"
-	log "github.com/sirupsen/logrus"
 )
 
 // AzureKeyVaultService provide interaction with Azure Key Vault
@@ -39,28 +36,12 @@ func (a *AzureKeyVaultService) GetSecret(secret *azureKeyVaultSecretv1alpha1.Azu
 }
 
 func (a *AzureKeyVaultService) getKeysClient(resource string) (*keyvault.BaseClient, error) {
-	keyClient := keyvault.New()
-
-	var authorizer autorest.Authorizer
-
-	var err error
-	if authorizer, err = auth.NewAuthorizerFromEnvironmentWithResource(resource); err != nil {
-		if err != nil {
-			log.Warning("azure: failed to get authorizer from environment, will try MSI directly")
-
-			msiEndpoint, err := adal.GetMSIVMEndpoint()
-			if err != nil {
-				return nil, fmt.Errorf("azure: failed to get msiendpoint, %+v", err)
-			}
-
-			spt, err := adal.NewServicePrincipalTokenFromMSI(msiEndpoint, resource)
-			if err != nil {
-				return nil, fmt.Errorf("failed to acquire a token using the MSI VM extension, Error: %+v", err)
-			}
-			authorizer = autorest.NewBearerAuthorizer(spt)
-		}
+	authorizer, err := auth.NewAuthorizerFromEnvironmentWithResource(resource)
+	if err != nil {
+		return nil, err
 	}
 
+	keyClient := keyvault.New()
 	keyClient.Authorizer = authorizer
 
 	return &keyClient, nil
