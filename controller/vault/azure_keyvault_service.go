8c39f3fbc74ab3d798e4f38df71cd10d3fe3e349
Jon Arild Tørresdal <jon.torresdal@spv.no>
2019-02-01 19:28:21 +0100

    Trying to base64 decode certificate

--- controller/vault/azure_keyvault_service.go
+++ controller/vault/azure_keyvault_service.go
@@ -2,6 +2,7 @@ package vault
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"strings"
 
@@ -39,6 +40,11 @@ func (a *AzureKeyVaultService) GetSecret(secret *azureKeyVaultSecretv1alpha1.Azu
 			return "", err
 		}
 		vaultSecret = string(*secretBundle.Cer)
+		decSecret, err := base64.StdEncoding.DecodeString(vaultSecret)
+		if err != nil {
+			return "", fmt.Errorf("failed to base64 decode Azure Key Vault certificate for %s / %s, error: %+v", secret.Namespace, secret.Name, err)
+		}
+		vaultSecret = string(decSecret)
 	default:
 		secretBundle, err := vaultClient.GetSecret(context.Background(), baseURL, secret.Spec.Vault.ObjectName, "")
 
