f6a442b51ad2225d498499543bdba636ad262809
Jon Arild Tørresdal <jon.torresdal@spv.no>
2019-02-02 10:44:02 +0100

    Trying to convert x509 to pem

--- controller/vault/azure_keyvault_service.go
+++ controller/vault/azure_keyvault_service.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"strings"
 
-	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/pkcs12"
 
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/2016-10-01/keyvault"
@@ -39,99 +38,103 @@ func (a *AzureKeyVaultService) GetSecret(secret *azureKeyVaultSecretv1alpha1.Azu
 
 	switch strings.ToLower(secret.Spec.Vault.ObjectType) {
 	case "certificate":
-		secretBundle, err := vaultClient.GetCertificate(context.Background(), baseURL, secret.Spec.Vault.ObjectName, "")
+		secretBundle, err := vaultClient.GetSecret(context.Background(), baseURL, secret.Spec.Vault.ObjectName, "")
 
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("failed to get secret from azure key vault, error: %+v", err)
 		}
 
-		cert, err := x509.ParseCertificate(*secretBundle.Cer)
-		if err != nil {
-			return "", fmt.Errorf("failed to parse certificate from Azure Key Vault, error: %+v", err)
-		}
+		keyPair := *secretBundle.Value
 
-		pubKey := string(CertToPEM(cert))
-
-		keyBundle, err := vaultClient.GetKey(context.Background(), baseURL, secret.Spec.Vault.ObjectName, "")
-		if err != nil {
-			return "", fmt.Errorf("failed to get certificate key from azure key vault, error: %+v", err)
-		}
-
-		log.Infof("private key format: %s", keyBundle.Key.Kty)
-
-		// nb, err := base64.RawURLEncoding.DecodeString(*keyBundle.Key.N)
+		return keyPair, nil
+		// cert, err := x509.ParseCertificate(*secretBundle.Cer)
 		// if err != nil {
-		// 	log.Errorf("failed to decode base64: %+v", err)
-		// }
-
-		log.Infof("n: %s", *keyBundle.Key.N)
-
-		// pk := &rsa.PublicKey{
-		// 	N: new(big.Int).SetBytes(nb),
-		// }
-
-		pk, err := x509.ParsePKCS1PrivateKey([]byte(*keyBundle.Key.N))
-		if err != nil {
-			log.Errorf("failed to parse pkcs1 private key: %+v", err)
-		}
-
-		// log.Infof("pkcs1 private key: %s", pk)
-
-		// privKeyDer, err := x509.ParsePKCS1PrivateKey(*keyBundle.Key.N)
-		// if err != nil {
-		// 	log.Errorf("failed to parse private key from Azure Key Vault, error: %+v", err)
-		// }
-		privKeyPem := x509.MarshalPKCS1PrivateKey(pk)
-		log.Infof("private key: %s", privKeyPem)
-
-		// rsaPrivateKeyBase64Decoded, err := base64.RawURLEncoding.DecodeString(*keyBundle.Key.N)
-		// if err != nil {
-		// 	return "", fmt.Errorf("failed to decode base64 private from Azure Key Vault key bundle, error: %+v", err)
-		// }
-		// log.Infof("base64 private key decoded: %s", rsaPrivateKeyBase64Decoded)
-		//
-		// rsaPrivateKey, err := x509.ParsePKCS1PrivateKey(rsaPrivateKeyBase64Decoded)
-		// if err != nil {
-		// 	return "", fmt.Errorf("failed to parse private key, error: %+v", err)
+		// 	return "", fmt.Errorf("failed to parse certificate from Azure Key Vault, error: %+v", err)
 		// }
 		//
-		// privateKey := string(x509.MarshalPKCS1PrivateKey(rsaPrivateKey))
-		// privateKey := string(base64.RawStdEncoding.Encode(dst, src) privateKeyByte)
-
-		// privateKey, err := x509.ParsePKCS8PrivateKey(cert.RawTBSCertificate)
+		// pubKey := string(CertToPEM(cert))
+		//
+		// keyBundle, err := vaultClient.GetKey(context.Background(), baseURL, secret.Spec.Vault.ObjectName, "")
 		// if err != nil {
-		// 	return "", fmt.Errorf("failed to parse pkcs8 private key, error: %+v", err)
+		// 	return "", fmt.Errorf("failed to get certificate key from azure key vault, error: %+v", err)
 		// }
 		//
-		// privBytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
+		// log.Infof("private key format: %s", keyBundle.Key.Kty)
+		//
+		// // nb, err := base64.RawURLEncoding.DecodeString(*keyBundle.Key.N)
+		// // if err != nil {
+		// // 	log.Errorf("failed to decode base64: %+v", err)
+		// // }
+		//
+		// log.Infof("n: %s", *keyBundle.Key.N)
+		//
+		// // pk := &rsa.PublicKey{
+		// // 	N: new(big.Int).SetBytes(nb),
+		// // }
+		//
+		// rsa.
+		// pk, err := x509.ParsePKCS1PrivateKey([]byte(*keyBundle.Key.N))
 		// if err != nil {
-		// 	return "", fmt.Errorf("failed to marshal pkcs8 private key, error: %+v", err)
+		// 	log.Errorf("failed to parse pkcs1 private key: %+v", err)
 		// }
 		//
-		// pemdata := pem.EncodeToMemory(&pem.Block{
-		// 	Type:  "RSA PRIVATE KEY",
-		// 	Bytes: privBytes,
-		// },
-		// )
-
-		return string(pubKey), nil
-
-		// return fmt.Printf(privateKey), nil
-
-		// pubKey := cert.PublicKey.(*rsa.PublicKey)
-
-		// switch cert.PublicKeyAlgorithm {
-		// case x509.RSA:
-		// }
-		// cert.PublicKey
-		// vaultSecret = cert.Subject.String()
-		// vaultClient.GetKey(context.Background(), baseURL, secretBundle.Kid, keyVersion)
-		// vaultSecret = bytes.NewBuffer(*secretBundle.Cer).String() //string(*secretBundle.Cer)
-		// decSecret, err := base64.StdEncoding.DecodeString(vaultSecret)
-		// if err != nil {
-		// 	return "", fmt.Errorf("failed to base64 decode Azure Key Vault certificate for %s / %s, error: %+v", secret.Namespace, secret.Name, err)
-		// }
-		// vaultSecret = string(decSecret)
+		// // log.Infof("pkcs1 private key: %s", pk)
+		//
+		// // privKeyDer, err := x509.ParsePKCS1PrivateKey(*keyBundle.Key.N)
+		// // if err != nil {
+		// // 	log.Errorf("failed to parse private key from Azure Key Vault, error: %+v", err)
+		// // }
+		// privKeyPem := x509.MarshalPKCS1PrivateKey(pk)
+		// log.Infof("private key: %s", privKeyPem)
+		//
+		// // rsaPrivateKeyBase64Decoded, err := base64.RawURLEncoding.DecodeString(*keyBundle.Key.N)
+		// // if err != nil {
+		// // 	return "", fmt.Errorf("failed to decode base64 private from Azure Key Vault key bundle, error: %+v", err)
+		// // }
+		// // log.Infof("base64 private key decoded: %s", rsaPrivateKeyBase64Decoded)
+		// //
+		// // rsaPrivateKey, err := x509.ParsePKCS1PrivateKey(rsaPrivateKeyBase64Decoded)
+		// // if err != nil {
+		// // 	return "", fmt.Errorf("failed to parse private key, error: %+v", err)
+		// // }
+		// //
+		// // privateKey := string(x509.MarshalPKCS1PrivateKey(rsaPrivateKey))
+		// // privateKey := string(base64.RawStdEncoding.Encode(dst, src) privateKeyByte)
+		//
+		// // privateKey, err := x509.ParsePKCS8PrivateKey(cert.RawTBSCertificate)
+		// // if err != nil {
+		// // 	return "", fmt.Errorf("failed to parse pkcs8 private key, error: %+v", err)
+		// // }
+		// //
+		// // privBytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
+		// // if err != nil {
+		// // 	return "", fmt.Errorf("failed to marshal pkcs8 private key, error: %+v", err)
+		// // }
+		// //
+		// // pemdata := pem.EncodeToMemory(&pem.Block{
+		// // 	Type:  "RSA PRIVATE KEY",
+		// // 	Bytes: privBytes,
+		// // },
+		// // )
+		//
+		// return string(pubKey), nil
+		//
+		// // return fmt.Printf(privateKey), nil
+		//
+		// // pubKey := cert.PublicKey.(*rsa.PublicKey)
+		//
+		// // switch cert.PublicKeyAlgorithm {
+		// // case x509.RSA:
+		// // }
+		// // cert.PublicKey
+		// // vaultSecret = cert.Subject.String()
+		// // vaultClient.GetKey(context.Background(), baseURL, secretBundle.Kid, keyVersion)
+		// // vaultSecret = bytes.NewBuffer(*secretBundle.Cer).String() //string(*secretBundle.Cer)
+		// // decSecret, err := base64.StdEncoding.DecodeString(vaultSecret)
+		// // if err != nil {
+		// // 	return "", fmt.Errorf("failed to base64 decode Azure Key Vault certificate for %s / %s, error: %+v", secret.Namespace, secret.Name, err)
+		// // }
+		// // vaultSecret = string(decSecret)
 	default:
 		secretBundle, err := vaultClient.GetSecret(context.Background(), baseURL, secret.Spec.Vault.ObjectName, "")
 
