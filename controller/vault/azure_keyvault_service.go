806a762d84d10b455b347f629e176193cb26e400
Jon Arild Tørresdal <jon.torresdal@spv.no>
2019-02-02 09:36:34 +0100

    Trying to convert x509 to pem

--- controller/vault/azure_keyvault_service.go
+++ controller/vault/azure_keyvault_service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/pem"
 	"fmt"
 	"strings"
@@ -45,14 +46,6 @@ func (a *AzureKeyVaultService) GetSecret(secret *azureKeyVaultSecretv1alpha1.Azu
 			return "", err
 		}
 
-		// x509.MarshalPKCS1PrivateKey(key)
-		privKeyDer, err := x509.ParsePKCS1PrivateKey(*secretBundle.Cer)
-		if err != nil {
-			log.Errorf("failed to parse private key from Azure Key Vault, error: %+v", err)
-		}
-		privKeyPem := x509.MarshalPKCS1PrivateKey(privKeyDer)
-		log.Infof("private key: %s", privKeyPem)
-
 		cert, err := x509.ParseCertificate(*secretBundle.Cer)
 		if err != nil {
 			return "", fmt.Errorf("failed to parse certificate from Azure Key Vault, error: %+v", err)
@@ -60,10 +53,37 @@ func (a *AzureKeyVaultService) GetSecret(secret *azureKeyVaultSecretv1alpha1.Azu
 
 		pubKey := string(CertToPEM(cert))
 
-		// keyBundle, err := vaultClient.GetKey(context.Background(), baseURL, secret.Spec.Vault.ObjectName, "")
-		// if err != nil {
-		// 	return "", fmt.Errorf("failed to get certificate key from azure key vault, error: %+v", err)
+		keyBundle, err := vaultClient.GetKey(context.Background(), baseURL, secret.Spec.Vault.ObjectName, "")
+		if err != nil {
+			return "", fmt.Errorf("failed to get certificate key from azure key vault, error: %+v", err)
+		}
+
+		log.Infof("private key format: %s", keyBundle.Key.Kty)
+
+		nb, err := base64.RawURLEncoding.DecodeString(*keyBundle.Key.N)
+		if err != nil {
+			log.Errorf("failed to decode base64: %+v", err)
+		}
+
+		log.Infof("base64 decoded n: %s", nb)
+
+		// pk := &rsa.PublicKey{
+		// 	N: new(big.Int).SetBytes(nb),
 		// }
+
+		pk, err := x509.ParsePKCS1PrivateKey([]byte(nb))
+		if err != nil {
+			log.Errorf("failed to parse pkcs1 private key: %+v", err)
+		}
+
+		// log.Infof("pkcs1 private key: %s", pk)
+
+		// privKeyDer, err := x509.ParsePKCS1PrivateKey(*keyBundle.Key.N)
+		// if err != nil {
+		// 	log.Errorf("failed to parse private key from Azure Key Vault, error: %+v", err)
+		// }
+		privKeyPem := x509.MarshalPKCS1PrivateKey(pk)
+		log.Infof("private key: %s", privKeyPem)
 
 		// rsaPrivateKeyBase64Decoded, err := base64.RawURLEncoding.DecodeString(*keyBundle.Key.N)
 		// if err != nil {
