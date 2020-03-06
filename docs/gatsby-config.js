5afe900ece62cbe16a346591d03775e5587aff3c
Jon Arild Tørresdal <jon.torresdal@spv.no>
2020-03-06 15:17:18 +0100

    Added back analytics

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -4,6 +4,17 @@ module.exports = {
   pathPrefix: '/',
   plugins: [
     {
+      resolve: 'gatsby-plugin-gtag',
+      options: {
+        // your google analytics tracking id
+        trackingId: 'UA-136446489-2',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: false,
+      },
+    },
+    {
       resolve: 'gatsby-theme-apollo-docs',
       options: {
         ...themeOptions,
