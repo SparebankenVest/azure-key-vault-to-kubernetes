6b9b2869f7f2a9393fed4a360da63718b90a5d79
Jon Arild Tørresdal <jon.torresdal@spv.no>
2020-03-19 22:09:40 +0100

    bumped copyright year

--- cmd/azure-keyvault-auth-service/main.go
+++ cmd/azure-keyvault-auth-service/main.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 Sparebanken Vest
+// Copyright © 2020 Sparebanken Vest
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
