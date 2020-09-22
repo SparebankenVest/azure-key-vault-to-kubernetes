f08358e423db243188854db7badf7a205fe6c89d
Jon Arild Tørresdal <jon.torresdal@spv.no>
2020-09-22 23:20:29 +0200

    Added test for pulling docker image config with sha image notation

--- cmd/azure-keyvault-secrets-webhook/registry_test.go
+++ cmd/azure-keyvault-secrets-webhook/registry_test.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/containers/image/v5/types"
 	"github.com/ghodss/yaml"
-	log "github.com/sirupsen/logrus"
 )
 
 func TestDockerPull(t *testing.T) {
@@ -32,8 +30,37 @@ func TestDockerPull(t *testing.T) {
 
 	if len(cmd) == 0 {
 		t.Errorf("no entrypoint nor cmd found there is something wrong.")
-		fmt.Print(yaml.Marshal(manifest.Config))
+		t.Log(yaml.Marshal(manifest.Config))
 	}
 
-	log.Infof("cmd found: %v", cmd)
+	t.Logf("cmd found: %v", cmd)
+}
+
+func TestDockerPullWithShaImageNotation(t *testing.T) {
+	config.dockerImageInspectionTimeout = 20
+
+	opts := imageOptions{
+		image:        "spvest/azure-keyvault-webhook@sha256:38a78fde88bd3bf023606ac3a2219b0803457734cb2e7bb80b55d36450cc71f1",
+		credentials:  types.DockerAuthConfig{},
+		architecture: "amd64",
+		osChoice:     "linux",
+	}
+
+	manifest, err := opts.getConfigFromManifest()
+
+	if err != nil {
+		t.Errorf("failed somewhere in get manifest %+v", err)
+		return
+	}
+
+	var cmd []string
+	cmd = append(cmd, manifest.Config.Entrypoint...)
+	cmd = append(cmd, manifest.Config.Cmd...)
+
+	if len(cmd) == 0 {
+		t.Errorf("no entrypoint nor cmd found there is something wrong.")
+		t.Log(yaml.Marshal(manifest.Config))
+	}
+
+	t.Logf("cmd found: %v", cmd)
 }
