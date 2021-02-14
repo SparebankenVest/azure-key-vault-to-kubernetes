2254bc709aa269d5bb5c678ed013ea0ee410522a
Jon Arild Tørresdal <jon.torresdal@spv.no>
2021-02-14 21:13:22 +0100

    fix how labels are used with prometheus

--- cmd/azure-keyvault-controller/controller/azureKeyVaultSecret.go
+++ cmd/azure-keyvault-controller/controller/azureKeyVaultSecret.go
@@ -47,7 +47,7 @@ func (c *Controller) initAzureKeyVaultSecret() {
 
 			if c.akvsHasOutputDefined(akvs) {
 				klog.V(4).InfoS("adding to queue", "azurekeyvaultsecret", klog.KObj(akvs))
-				syncCounter.WithLabelValues("operation", "add", "object", "AzureKeyVaultSecret")
+				syncCounter.WithLabelValues("add", "AzureKeyVaultSecret")
 				queue.Enqueue(c.akvsCrdQueue.GetQueue(), obj)
 			}
 		},
@@ -67,14 +67,14 @@ func (c *Controller) initAzureKeyVaultSecret() {
 			// If akvs has not changed and has secret output, add to akv queue to check if secret has changed in akv
 			if newAkvs.ResourceVersion == oldAkvs.ResourceVersion && c.akvsHasOutputDefined(newAkvs) {
 				klog.V(4).InfoS("adding to azure key vault queue to check if secret has changed in azure key vault", "azurekeyvaultsecret", klog.KObj(newAkvs))
-				syncCounter.WithLabelValues("operation", "update", "object", "AzureKeyVault")
+				syncCounter.WithLabelValues("update", "AzureKeyVault")
 				queue.Enqueue(c.azureKeyVaultQueue.GetQueue(), new)
 				return
 			}
 
 			if c.akvsHasOutputDefined(newAkvs) || c.akvsHasOutputDefined(oldAkvs) {
 				klog.V(4).InfoS("azurekeyvaultsecret changed - adding to queue", "azurekeyvaultsecret", klog.KObj(newAkvs))
-				syncCounter.WithLabelValues("operation", "update", "object", "AzureKeyVaultSecret")
+				syncCounter.WithLabelValues("update", "AzureKeyVaultSecret")
 				queue.Enqueue(c.akvsCrdQueue.GetQueue(), new)
 			}
 		},
@@ -87,7 +87,7 @@ func (c *Controller) initAzureKeyVaultSecret() {
 
 			if c.akvsHasOutputDefined(akvs) {
 				klog.V(4).InfoS("azurekeyvaultsecret deleted - adding to queue", "azurekeyvaultsecret", klog.KObj(akvs))
-				syncCounter.WithLabelValues("operation", "delete", "object", "AzureKeyVaultSecret")
+				syncCounter.WithLabelValues("delete", "AzureKeyVaultSecret")
 				queue.Enqueue(c.akvsCrdQueue.GetQueue(), obj)
 
 				err = c.deleteKubernetesValues(akvs)
