e5bff41c702bcc931cd5e262ca7ea40de712151c
YouJinTou <dandreevd@gmail.com>
2023-06-07 16:53:18 +0300

    Added an empty line.

--- cmd/azure-keyvault-secrets-webhook/pod.go
+++ cmd/azure-keyvault-secrets-webhook/pod.go
@@ -281,6 +281,7 @@ func (p podWebHook) mutatePodSpec(ctx context.Context, pod *corev1.Pod) error {
 	podSpec.SecurityContext = &corev1.PodSecurityContext{
 		RunAsNonRoot: &[]bool{viper.GetBool("webhook_pod_spec_security_context_non_root")}[0],
 	}
+
 	if p.useAuthService {
 		klog.InfoS("creating client certificate to use with auth service", klog.KRef(p.namespace, pod.Name))
 		authServiceSecret, err = p.authService.NewPodSecret(pod, p.namespace, p.mutationID)
